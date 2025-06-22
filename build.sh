7819cec3cebb3609bba538b1e495d4d616e855e1
Arpan Adlakhiya <arpanadlakhiya@gmail.com>
2025-06-22 09:14:57 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Exit on error
 set -e
 
+echo "Installing dependencies..."
+curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash
+
 echo "Building Solana program..."
 cd solana-program
 npm install
